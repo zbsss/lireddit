72a77bf4de16894e221e78cfa12bc4b9240ce054
Michał Kurleto <mkurleto@qualtrics.com>
2021-09-01 20:04:40 +0200

    Configure graphql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -12,8 +16,24 @@ const main = async () => {
   // const post = orm.em.create(Post, { title: 'my first post' });
   // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      validate: false,
+      resolvers: [HelloResolver, PostResolver],
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(5000, () => {
+    console.log('Application started on localhost:4000');
+  });
 };
 
 main();
